5279c093270b1fa9c4b1cf1475eef116cd4173f8
Nguyen Thanh <nvthanh12102004@gmail.com>
2024-05-06 14:20:56 +0700

    update search

---
+++ src/main/java/data/mining/LevenshteinDistance.java
@@ -0,0 +1,34 @@
+package data.mining;
+
+import java.util.Arrays;
+
+public class LevenshteinDistance {
+
+    public static int calculateDistance(String str1, String str2){
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        int m = str1.length(), n = str2.length();
+        int[] prevRow = new int[n + 1] , currRow = new int[n + 1];
+
+        for ( int i = 0 ; i < n ; i++){
+            prevRow[i] = i;
+        }
+
+        for ( int i = 1 ; i <= m ; i++){
+            currRow[0] = i;
+            for ( int j = 1 ; j <= n ; j++){
+                if ( str1.charAt(i - 1) == str2.charAt(j - 1) ){
+                    currRow[j] = prevRow[j - 1];
+                }
+                else{
+                    currRow[j] = 1 + Math.min(currRow[j - 1], Math.min(prevRow[j], prevRow[j - 1]));
+                }
+            }
+
+            prevRow = Arrays.copyOf(currRow, currRow.length );
+        }
+
+        return currRow[n];
+    }
+
+}
--- src/main/java/data/mining/Search.java
+++ src/main/java/data/mining/Search.java
@@ -1,74 +1,73 @@
 package data.mining;
-
 import com.google.gson.Gson;
-import edu.stanford.nlp.simple.*;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
+
+
 public class Search {
+
+    private final static double THRESHOLD = 0.75;
+
     public static double calculateSimilarity(String query, String title) {
-        // Use lib spaCy to data preprocessing
-        Document doc1 = new Document(query);
-        Document doc2 = new Document(title);
-
-        // Get words from preprocessed text
-        List<String> queryTokens = new ArrayList<>();
-        List<String> titleTokens = new ArrayList<>();
-        for (Sentence sent : doc1.sentences()) {
-            queryTokens.addAll(sent.words());
+        Map<String, Integer > queryVector = vectorizeText(query);
+        Map<String, Integer > titleVector = vectorizeText(title);
+        Map<String , Integer > similarityVector = new HashMap<>();
+        int numberOfWordsInTitle = 0;
+        int countSimilarity = 0;
+        for ( Map.Entry<String, Integer> entryQuery : queryVector.entrySet()) {
+            for ( Map.Entry<String, Integer> entryTitle : titleVector.entrySet()) {
+                if ( 1 - LevenshteinDistance.calculateDistance( entryTitle.getKey() , entryQuery.getKey())*1.0 / entryTitle.getKey().length() >= THRESHOLD ){
+                    similarityVector.put(entryTitle.getKey(), similarityVector.getOrDefault(entryTitle.getKey(),0) + 1);
+                }
+            }
         }
-        System.out.println(queryTokens);
-        for (Sentence sent : doc2.sentences()) {
-            titleTokens.addAll(sent.words());
-        }
-        System.out.println(titleTokens);
 
-        // Use Jaccard to caculate the similarity
-        Set<String> intersection = new HashSet<>(queryTokens);
-        intersection.retainAll(titleTokens);
-        double intersectionSize = intersection.size();
-        System.out.println("intersectionSize: " + intersectionSize);
-        double unionSize = queryTokens.size() + titleTokens.size() - intersectionSize;
-        System.out.println("Union size: " + unionSize);
-        return intersectionSize / unionSize;
+        for ( Map.Entry<String, Integer> entryTitle : titleVector.entrySet()) {
+            numberOfWordsInTitle += entryTitle.getValue();
+            countSimilarity += Math.min(similarityVector.getOrDefault(entryTitle.getKey() , 0) , entryTitle.getValue());
+        }
+
+        return countSimilarity*1.0/numberOfWordsInTitle;
     }
 
-    public static List<String> tokenize(String text) {
-        // Implement your tokenization logic here
-        return Arrays.asList(text.split("\\s+"));
+
+    public static Map<String, Integer> vectorizeText(String text) {
+        Map<String, Integer> vector = new HashMap<>();
+        String[] words = text.split("\\s+");
+
+        for (String word : words) {
+            vector.put(word, vector.getOrDefault(word, 0) + 1);
+        }
+
+        return vector;
     }
 
     public static void main(String[] args) {
-        //Take data from JSON file
         Gson gson = new Gson();
         List<Aggreator> aggreatorList = null;
-        try (FileReader reader = new FileReader("C://Users/ADMIN/IdeaProjects/News-aggregator/src/main/java/data/mining/data.json")) {
+        try (FileReader reader = new FileReader("src/main/resources/data.json")) {
             Aggreator[] aggreatorArray = gson.fromJson(reader, Aggreator[].class);
             aggreatorList = Arrays.asList(aggreatorArray);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //Get the title you want to search for from GUI
-        String query = "block";
-
-        // Data preprocessing and calculate the similarity
-            Map<Aggreator, Double> similarityScores = new HashMap<>();
-        for (Aggreator aggreator  : aggreatorList) {
-            double similarity = calculateSimilarity(query, aggreator.getTitle());
-            similarityScores.put( aggreator , similarity);
+        String query = "Blockchain";
+        Map<Aggreator, Double> similarityScores = new HashMap<>();
+        for (Aggreator aggreator : aggreatorList) {
+            double similarity = calculateSimilarity(aggreator.getTitle() , query );
+            similarityScores.put(aggreator, similarity);
         }
 
-        // Sort result
         List<Map.Entry<Aggreator, Double>> sortedResults = new ArrayList<>(similarityScores.entrySet());
         sortedResults.sort((entry1, entry2) -> Double.compare(entry2.getValue(), entry1.getValue()));
 
-        // Give sortedResults for GUI
-        int index = 0;
-//        for( Map.Entry<Aggreator,Double> entry : sortedResults){
-//            System.out.println("Title " + index +  " : " + entry.getKey().getTitle() + " : " + entry.getValue());
-//            index++;
-//        }
+        for (Map.Entry<Aggreator, Double> entry : sortedResults) {
+            System.out.println("Tiêu đề: " + entry.getKey().getTitle() + ", Điểm tương đồng: " + entry.getValue());
+        }
     }
 }
